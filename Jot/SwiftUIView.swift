81ef1898982f854cbc0eccefcde5ef2f9375f760
Brian Goodwin <brianjgoodwin@gmail.com>
2023-12-25 18:25:20 -0800

    Attempting to fix version control issue

---
+++ Jot/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Jot
+//
+//  Created by Brian on 12/25/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
